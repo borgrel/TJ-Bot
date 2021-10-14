27ab22e811d76b94705f5e1aa10d9d3eeecf650a
Zabuzard <zabuza.dev@gmail.com>
2021-10-14 10:52:32 +0200

    Removed "@author" tags

--- formatter/src/main/java/org/togetherjava/formatter/tokenizer/TokenizationException.java
+++ formatter/src/main/java/org/togetherjava/formatter/tokenizer/TokenizationException.java
@@ -2,8 +2,6 @@ package org.togetherjava.formatter.tokenizer;
 
 /**
  * Exception that can occur when lexing
- *
- * @author illuminator3
  */
 public class TokenizationException extends RuntimeException {
     public TokenizationException(String message) {
